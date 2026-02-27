804d2a1dc21c87ab17874f771aab240ddb70716d
Konstantin Voykov <koykov0@gmail.com>
2026-02-27 23:43:44 +0300

    scalar: parse infinite numbers v2

--- vector_test.go
+++ vector_test.go
@@ -87,6 +87,11 @@ func TestScalar(t *testing.T) {
 		assertType(t, vec, "", vector.TypeNumber)
 		assertNumber(t, vec, "", math.Inf(1))
 	})
+	t.Run("scalar_number_Inf", func(t *testing.T) {
+		vec = assertParse(t, vec, nil, 0)
+		assertType(t, vec, "", vector.TypeNumber)
+		assertNumber(t, vec, "", math.Inf(1))
+	})
 	t.Run("scalar_string", func(t *testing.T) {
 		vec = assertParse(t, vec, nil, 0)
 		assertType(t, vec, "", vector.TypeString)
